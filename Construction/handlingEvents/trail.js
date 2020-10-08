8e1ea986044fb06f0b83a8300cf6f5c82f7970f7
Ivan Esmantovich <esmantovich@protonmail.com>
2020-10-08 15:49:32 +0300

    Chapter 15 (In Progress)

---
+++ Construction/handlingEvents/trail.js
@@ -0,0 +1,25 @@
+// window.addEventListener('mousemove', event => {
+// 	let shadow = document.createElement('div')
+// 	shadow.className = 'trail'
+// 	shadow.style.left = (event.pageX + 4) + 'px'
+// 	shadow.style.top = (event.pageY + 4) + 'px'
+// 	setTimeout(() => {
+// 		shadow.remove()
+// 	}, 100);
+// 	document.body.appendChild(shadow)
+// })
+
+let shadows = []
+for (let i = 0; i < 12; i++) {
+	let shadow = document.createElement('div')
+	shadow.className = 'trail'
+	shadows.push(shadow)
+	document.body.appendChild(shadow)
+}
+let currentShadow = 0
+window.addEventListener('mousemove', event => {
+	shadows[currentShadow].style.left = (event.pageX + 4) + 'px'
+	shadows[currentShadow].style.top = (event.pageY + 4) + 'px'
+	currentShadow = (currentShadow + 1) % shadows.length
+})
+
